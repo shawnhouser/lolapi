ecde156b7965659c4724660b321ad770fb95d889
Shawn Houser <shawnh@17-Houser-Shawn.local>
2017-04-26 20:30:30 -0400

    Changed Rate Limiter around for a number of reasons. Old system was inconsistent, confusing, slow, and would cause problems if there was a large gap of no calls New system uses a setInterval, and is much simpiler, and has less problems, but has less flexibility. Possibly update in future

--- test/rateLimiter.spec.js
+++ test/rateLimiter.spec.js
@@ -6,23 +6,6 @@ describe('util', function () {
 
   describe('#rateLimiter', function () {
 
-    it('should limit to 10 calls per 500ms', function (done) {
-      var limiter = new RateLimiter(10, 500);
-      var startedOn = Date.now();
-      var count = 0;
-
-      for (var i = 0; i < 21; i++) {
-        limiter.schedule(function () {
-          expect(Date.now()).to.be.at.least(startedOn + count * 50);
-
-          if (++count === 21) {
-            done();
-          };
-        });
-      }
-
-    });
-
     it('should return featured games 11 times without failure', function (done) {
       var count = 0;
 
